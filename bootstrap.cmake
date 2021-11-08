cb191317906cbf7657714c0d51c21d7326374313
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-11-08 14:34:32 -0700

    Update bootstrap.cmake

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -37,12 +37,22 @@ else()
   execute_process(
     COMMAND cmake .. -G${GENERATOR}
     WORKING_DIRECTORY ${CMAKE_SDK_DIRECTORY}/${BUILD_DIR}
+    RESULT_VARIABLE CMAKE_SDK_CMAKE_RESULT
   )
+
+  if(${CMAKE_SDK_CMAKE_RESULT})
+    message(FATAL_ERROR "Failed to run cmake for CMakeSDK project")
+  endif()
 
   execute_process(
     COMMAND cmake --build . --target install
     WORKING_DIRECTORY ${CMAKE_SDK_DIRECTORY}/${BUILD_DIR}
+    RESULT_VARIABLE CMAKE_SDK_INSTALL_RESULT
   )
+
+  if(${CMAKE_SDK_INSTALL_RESULT})
+    message(FATAL_ERROR "Failed to install CMakeSDK project")
+  endif()
 
   file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR})
 
@@ -52,22 +62,43 @@ else()
     execute_process(
       COMMAND cmake -DSDK_IS_TEST=ON ..
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR}
+      RESULT_VARIABLE CMAKE_SDK_RESULT
     )
+
+    if(${CMAKE_SDK_RESULT})
+      message(FATAL_ERROR "Failed to run CMake")
+    endif()
 
     execute_process(
       COMMAND cmake --build . --target all -- -j8
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR}
+      RESULT_VARIABLE BUILD_SDK_RESULT
     )
+
+    if(${BUILD_SDK_RESULT})
+      message(FATAL_ERROR "Failed to build")
+    endif()
 
     execute_process(
       COMMAND cmake --build . --target API_test -- -j8
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR}
+      RESULT_VARIABLE BUILD_API_TEST_RESULT
     )
+
+    if(${BUILD_API_TEST_RESULT})
+      message(FATAL_ERROR "Failed to build API test")
+    endif()
 
     execute_process(
       COMMAND ctest -VV
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR}
+      RESULT_VARIABLE TEST_OUTPUT_RESULT
     )
+
+    if(${TEST_OUTPUT_RESULT})
+      message(FATAL_ERROR "Test failed to complete successfully")
+    endif()
+
   endif()
 endif()
 

892e190bdc6e364e8790b591fc99c04030255f07
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-07-12 22:11:00 -0600

    update bootstrap script

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -4,6 +4,8 @@ set(DEPENDENCIES_DIRECTORY ${SDK_DIRECTORY}/dependencies)
 file(MAKE_DIRECTORY ${DEPENDENCIES_DIRECTORY})
 file(REMOVE_RECURSE ${DEPENDENCIES_DIRECTORY}/CMakeSDK)
 
+message(STATUS "cloning CMakeSDK project")
+
 execute_process(
   COMMAND git clone --branch dev/v2.0 https://github.com/StratifyLabs/CMakeSDK.git
   WORKING_DIRECTORY ${DEPENDENCIES_DIRECTORY}
@@ -22,11 +24,55 @@ else()
   set(BOOTSTRAP_SCRIPT bootstrap)
 endif()
 
+
+message(STATUS "Running ${BOOTSTRAP_SCRIPT} using SDK path: ${SDK_DIRECTORY}")
+
 execute_process(
   COMMAND cmake -DCMSDK_SDK_PATH=${SDK_DIRECTORY} -P ${DEPENDENCIES_DIRECTORY}/CMakeSDK/scripts/${BOOTSTRAP_SCRIPT}.cmake
 )
 
 if(IS_BUILD_AND_TEST)
+  message(STATUS "Build and run API Tests")
+
+  execute_process(
+    COMMAND cmake -DSDK_IS_TEST=ON ..
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR}
+    RESULT_VARIABLE CMSDK_RESULT
+  )
+
+  if(${CMSDK_RESULT})
+    message(FATAL_ERROR "Failed to run CMake")
+  endif()
+
+  execute_process(
+    COMMAND cmake --build . --target all -- -j8
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR}
+    RESULT_VARIABLE BUILD_SDK_RESULT
+  )
+
+  if(${BUILD_SDK_RESULT})
+    message(FATAL_ERROR "Failed to build")
+  endif()
+
+  execute_process(
+    COMMAND cmake --build . --target API_test -- -j8
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR}
+    RESULT_VARIABLE BUILD_API_TEST_RESULT
+  )
+
+  if(${BUILD_API_TEST_RESULT})
+    message(FATAL_ERROR "Failed to build API test")
+  endif()
+
+  execute_process(
+    COMMAND ctest -VV
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${BUILD_DIR}
+    RESULT_VARIABLE TEST_OUTPUT_RESULT
+  )
+
+  if(NOT ${TEST_OUTPUT_RESULT} EQUAL "0")
+    message(FATAL_ERROR "Test failed to complete successfully")
+  endif()
 
 endif()
 
